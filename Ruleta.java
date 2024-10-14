5bc43480e2e4e0ca9aba35822832d98c9bbd4e4d
roomeroo05 <roomeroo05@gmail.com>
2024-10-14 22:59:16 +0400

    arreglo cosas

--- Ruleta.java
+++ Ruleta.java
@@ -1,88 +1,87 @@
 package ruleta;
+
 import java.util.Scanner;
 import java.lang.Math;
-public class Ruleta
-{
-    
-    public static void main(String[] arguments)
-    {
+import ruleta.src.consoleColors;
+
+public class Ruleta {
+
+    public static void main(String[] arguments) {
         String apuesta = "";
         int payout = 0;
         int bolita = 0;
         int casilla = -1;
         String retry = "si";
         Scanner s = new Scanner(System.in);
-        
-        do
-        {
+
+        do {
             System.out.println("¿Cual es tu apuesta?");
-            System.out.println("Inserte rojo, negro, numero");    
-            do
-            {
+            System.out.println(
+                    "Inserte " + consoleColors.RED + "rojo" + consoleColors.RESET + "," + consoleColors.BLACK + "negro"
+                            + consoleColors.RESET + " o " + consoleColors.GREEN + "numero" + consoleColors.RESET + ".");
+            do {
                 apuesta = System.console().readLine();
-                if(!apuesta.equals("rojo") && !apuesta.equals("negro") && !apuesta.equals("numero"))
-                {    
-                    System.out.println("Que digas rojo, negro o numero tontito");
+                if (!apuesta.equals("rojo") && !apuesta.equals("negro") && !apuesta.equals("numero")) {
+                    System.out.println("Que digas " + consoleColors.RED + "rojo" + consoleColors.RESET + ", "
+                            + consoleColors.BLACK + "negro" + consoleColors.RESET + " o " + consoleColors.GREEN
+                            + "numero" + consoleColors.RESET);
                 }
 
-            }while(!apuesta.equals("rojo") && !apuesta.equals("negro") && !apuesta.equals("numero"));
-            
-            if(apuesta.equals("numero"))
-            {
-                do
-                {
-                    System.out.println("¿Cual es tu numero master?(0-36)");
-                    casilla = s.nextInt();
-                }while(casilla >36 || casilla < 0);
+            } while (!apuesta.equals("rojo") && !apuesta.equals("negro") && !apuesta.equals("numero"));
+
+            if (apuesta.equals("numero")) {
+                do {
+                    System.out.println("¿Cual es tu " + consoleColors.GREEN + "numero" + consoleColors.RESET + ", "
+                            + consoleColors.PURPLE + "master" + consoleColors.RESET + "?(0-36)");
+                    casilla = Integer.parseInt(System.console().readLine());
+                } while (casilla > 36 || casilla < 0);
             }
             System.out.println("¿Cuánto dinero apuestas?");
-            Float dinerin = s.nextFloat();
-            do
-            {
-                bolita = (int) (Math.random() * 100) ;
-            }while(bolita > 36);
-            System.out.println("La bolita de los cojones ha caido en el " + bolita + ".");
+            Float dinerin = Float.parseFloat(System.console().readLine());
+            do {
+                bolita = (int) (Math.random() * 100);
+            } while (bolita > 36);
+            System.out.println("La bolita ha caido en el " + bolita + ".");
 
-            switch (apuesta) 
-            {
+            switch (apuesta) {
                 case "rojo":
                     payout = 2;
-                    if(bolita % 2 == 0 && bolita != 0)
-                        {
-                            System.out.println("Ha tocado rojo, enhorabuena tu premio es de " + payout*dinerin + "$.");
-                        }
-                        else
-                            System.out.println("jajaja perdiste");
-                        break;
+                    if (bolita % 2 == 0 && bolita != 0) {
+                        System.out.println("Ha tocado " + consoleColors.RED + "rojo" + consoleColors.RESET
+                                + ", enhorabuena tu " + consoleColors.YELLOW_BACKGROUND_BRIGHT + "premio"
+                                + consoleColors.RESET + " es de " + payout * dinerin + "$.");
+                    } else
+                        System.out.println("jajaja perdiste");
+                    break;
 
                 case "negro":
                     payout = 2;
-                        if (bolita %2 == 1)
-                        {
-                            System.out.println("Ha tocado negro, enhorabuena tu premio es de " + payout*dinerin + " $.");   
-                        }
-                        else
-                            System.out.println("jajaja perdiste");
+                    if (bolita % 2 == 1) {
+                        System.out.println("Ha tocado " + consoleColors.BLACK + "negro" + consoleColors.RESET
+                                + ", enhorabuena tu premio es de " + payout * dinerin + " $.");
+                    } else
+                        System.out.println("jajaja perdiste");
                     break;
-            
+
                 case "numero":
                     payout = 36;
-                    if(bolita == casilla)
-                    {  
-                        System.out.println("Enhorabuena, ha tocado su numero suertudo cabron, su premio es de " + payout*dinerin + " $.");
-                    }
-                    else
-                        System.out.println("Jajaja perdiste gordo mamon");
-                        break;
-                    
+                    if (bolita == casilla) {
+                        System.out.println("Enhorabuena, ha tocado su " + consoleColors.GREEN + "numero"
+                                + consoleColors.RESET + " suertudo, su premio es de "
+                                + payout * dinerin + " $.");
+                    } else
+                        System.out.println("Jajaja perdiste " + ".");
+                    break;
+
                 default:
-                    System.out.println("Hubo un error con la apuesta. No se que ha pasao máquina");
+                    System.out.println(consoleColors.PURPLE_BACKGROUND_BRIGHT
+                            + "Hubo un error con la apuesta. No se que ha pasao máquina" + consoleColors.RESET);
                     break;
             }
             System.out.println("\n---------------------------------------------------");
             System.out.println("!Quieres reintentar? si/no");
             retry = System.console().readLine();
-        }while(retry.equals("si"));
+        } while (retry.equals("si"));
         s.close();
     }
 }
